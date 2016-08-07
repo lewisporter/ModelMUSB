526cd0f803fe47f93e60f7e515b74a66494d0caf
Lewis Porter <lewis.porter@gmail.com>
2016-08-07 17:01:11 +0200

    Fixed broken ASF

--- ModelMUSB/src/ASF/sam/utils/compiler.h
+++ ModelMUSB/src/ASF/sam/utils/compiler.h
@@ -582,7 +582,7 @@ typedef struct
  * \return The count of leading zero bits in \a u.
  */
 #if (defined __GNUC__) || (defined __CC_ARM)
-#   define clz(u)              ((u) ? __builtin_ctz(u) : 32)
+#   define clz(u)              ((u) ? __builtin_clz(u) : 32)
 #elif (defined __ICCARM__)
 #   define clz(u)              ((u) ? __CLZ(u) : 32)
 #else
